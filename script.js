8b25b054097db714896340b5485df9c15008515e
victoriaabramovich <abramovichvictoria@gmail.com>
2023-11-20 20:03:48 +0200

    burger-bar

--- script.js
+++ script.js
@@ -1,50 +1,5 @@
-// "use strict";
+ "use strict";
 
-// let inputElement = document.getElementById('input-box');
-// let addBtn = document.querySelector('.Click');
-
-// let ulElementItems = document.querySelector('ul-list-items');
-
-// addBtn.addEventListener('click',function(){
-// let value = inputElement.value;
-
-// let li = document.createElement('li');
-// let deleteBtn = document.createElement('button');
-// deleteBtn.textContent = "Delete This Item";
-
-// deleteBtn.addEventListener('click', function ())
-// li.remove();
-
-// li.textContent = value;
-
-
-
-// ulElementItems.appendChild('li');
-
-// inputElement.value = " ";
-
-//  })
-
-
-//  let inputElement = document.getElementById("input-box");
-// let addBtn = document.querySelector(".Click");
-
-// let ulElementItems = document.querySelector(".ul-list-items");
-
-// addBtn.addEventListener("click", function () {
-//   let value = inputElement.value;
-
-//   let li = document.createElement("li");
-//   let deleteBtn = document.createElement("button");
-//   deleteBtn.textContent = "Delete This Item";
-
-//   deleteBtn.addEventListener("click", function () {
-//     li.remove();
-//   });
-
-//   li.appendChild(deleteBtn);
-//   ulElementItems.appendChild(li);
-// });
 let inputElement = document.getElementById("input-box");
 let addBtn = document.querySelector(".Click");
 let ulElementItems = document.querySelector(".ul-list-items");
@@ -65,3 +20,14 @@ addBtn.addEventListener("click", function () {
 li.appendChild(deleteBtn);
   ulElementItems.appendChild(li);
 });
+
+// burger bar
+
+let navigationElement = document.getElementById ("navBar");
+let burgerElement = document.getElementById ("burgerBar");
+
+burgerElement.addEventListener('click', function() {
+    // navigationElement.classList.add('navActive');-damateba
+    navigationElement.classList.toggle('navActive');
+    
+})
